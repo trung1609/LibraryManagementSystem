05f52b845d4fcae98662a3d08f557bd96e46fdbd
trung1609 <vmt162005@gmail.com>
2026-01-25 09:30:21 +0700

    git init

---
+++ src/main/java/com/example/LibraryManagementSystem/Hello.java
@@ -0,0 +1,7 @@
+package com.example.LibraryManagementSystem;
+
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("Hello, Library Management System!");
+    }
+}

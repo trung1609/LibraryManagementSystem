1e03f828d17af0fa3fe28fff1eff0d9afe9bf7dd
Vũ Minh Trung <167429293+trung1609@users.noreply.github.com>
2026-01-28 20:04:07 +0700

    Delete LibraryManagementSystem directory

--- LibraryManagementSystem/src/main/java/com/example/LibraryManagementSystem/exception/GenreException.java
+++
@@ -1,7 +0,0 @@
-package com.example.LibraryManagementSystem.exception;
-
-public class GenreException extends Exception {
-    public GenreException(String message) {
-        super(message);
-    }
-}

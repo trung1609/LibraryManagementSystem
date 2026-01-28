845b11ad2ebe9d75ecc4b6ca1cee86ec544c3ca6
trung1609 <vmt162005@gmail.com>
2026-01-28 21:24:07 +0700

    Add JWT authentication support with JwtValidator and JwtConstant classes

---
+++ src/main/java/com/example/LibraryManagementSystem/config/JwtConstant.java
@@ -0,0 +1,6 @@
+package com.example.LibraryManagementSystem.config;
+
+public class JwtConstant {
+    public static final String JWT_HEADER = "Authorization";
+    public static final String SECRET_KEY = "kasldjaskldaskdasjkdjaskldjasldaadalsasldaskdl";
+}
--- src/main/java/com/example/LibraryManagementSystem/config/SecurityConfig.java
+++ src/main/java/com/example/LibraryManagementSystem/config/SecurityConfig.java
@@ -1,19 +1,59 @@
 package com.example.LibraryManagementSystem.config;
 
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Collections;
 
 @Configuration
 public class SecurityConfig {
+
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        http
-                .csrf(csrf -> csrf.disable())
-                .authorizeHttpRequests(auth -> auth
+        return http
+                .sessionManagement(management -> management.sessionCreationPolicy(
+                        SessionCreationPolicy.STATELESS
+                ))
+                .csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(Authorize -> Authorize
+                        .requestMatchers("/api/**").authenticated()
+                        .requestMatchers("/api/admin/**").hasRole("ADMIN")
                         .anyRequest().permitAll()
+                )
+                .addFilterBefore(new JwtValidator(), BasicAuthenticationFilter.class)
+                .cors(cors -> cors.configurationSource(corsConfigurationSource()))
+        .build();
+    }
+
+    private CorsConfigurationSource corsConfigurationSource(){
+        return new CorsConfigurationSource() {
+            @Override
+            public CorsConfiguration getCorsConfiguration(HttpServletRequest request) {
+                CorsConfiguration cfg = new CorsConfiguration();
+                cfg.setAllowCredentials(true);
+                cfg.setAllowedOrigins(
+                        Arrays.asList(
+                                "http://localhost:5173/",
+                                "https://trunglibrary.com"
+                        )
                 );
-        return http.build();
+                cfg.setAllowedMethods(Collections.singletonList("*"));
+                cfg.setAllowedHeaders(Collections.singletonList("*"));
+                cfg.setExposedHeaders(Collections.singletonList("Authorization"));
+                cfg.setMaxAge(360L);
+                return cfg;
+            }
+        };
     }
 }
